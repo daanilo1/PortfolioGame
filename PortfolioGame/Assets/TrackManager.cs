2a94283d01f9c7ce5378aa7e78e8ee9993cf77a3
daanilo1 <dmdanilo8@gmail.com>
2017-03-28 23:01:18 -0700

    first commit

---
+++ PortfolioGame/Assets/TrackManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrackManager : MonoBehaviour {
+    public GameObject[] trackPrefabs;
+    private Transform playerTransform;
+    private float spawnZ = -12.0f;
+    private float trackLenght = 15.0f;
+    private float safeZone = 20.0f;
+    private int tracksOnScreen = 7;
+
+    private List<GameObject> activeTracks;
+
+    // Use this for initialization
+    private void Start () {
+        activeTracks = new List<GameObject>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        for(int i = 0; i < tracksOnScreen; i++)
+        {
+            SpawnTrack();
+        }
+            
+    }
+	
+	// Update is called once per frame
+	private void Update () {
+        if(playerTransform.position.z - safeZone > (spawnZ - tracksOnScreen * trackLenght))
+        {
+            SpawnTrack();
+            DeleteTrack();
+        }
+	
+	}
+       private void SpawnTrack(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(trackPrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += trackLenght;
+        activeTracks.Add(go);
+    }
+
+    private void DeleteTrack()
+    {
+        Destroy(activeTracks[0]);
+        activeTracks.RemoveAt(0);
+    }
+}
